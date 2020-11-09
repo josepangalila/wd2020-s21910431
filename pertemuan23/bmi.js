5960259aa7b9b8c45df851ff69f58f870b77cb6b
jose pangalila <70064606+josepangalila@users.noreply.github.com>
2020-11-10 00:00:52 +0800

    Add files via upload

---
+++ pertemuan23/bmi.js
@@ -0,0 +1,10 @@
+let BeratBadan = prompt("Masukan berat : ");
+console.log("Berat anda adalah : " + BeratBadan);
+
+let TinggiBadan = prompt("Masukan tinggi : ");
+console.log("Tinggi anda adalah : " + TinggiBadan);
+
+let Bil1 = BeratBadan;
+let BIl2 = TinggiBadan;
+
+console.log(Bil1 / BIl2 * BIl2);
